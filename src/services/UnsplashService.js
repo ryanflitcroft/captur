9e968d7d90bd82b8f6c31ba547013d6a559795da
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-28 12:50:08 -0700

    UnsplashService function requestUnsplash updated.

--- src/services/UnsplashService.js
+++ src/services/UnsplashService.js
@@ -1,9 +1,9 @@
 import handleUnsplashData from '../utils/handleUnsplashData';
 
 export default async function requestUnsplash(query) {
-  const res = await fetch(`https://api.unsplash.com/photos/${query}`, {
+  const res = await fetch(`https://api.unsplash.com/photos/?${query}`, {
     headers: {
-      Authorization: `${process.env.UNSPLASH_ACCESS_KEY}`,
+      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
       'Content-Type': 'application/json',
     },
   });
