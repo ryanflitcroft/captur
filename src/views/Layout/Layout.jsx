3eeede035c5e64191e76fc5b7d201352b9f21905
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-28 10:06:21 -0700

    initial components Layout, Header, Main, Footer.

---
+++ src/views/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Layout/Header/Header';
+import Main from '../../components/Layout/Main/Main';
+import Footer from '../../components/Layout/Footer/Footer';
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
