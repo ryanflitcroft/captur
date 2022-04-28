0b44cf854b3a2a50ca1c94aa5097f5d9ed013773
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-28 12:12:10 -0700

    component CollectionItem written.

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default function CollectionItem({ item }) {
+  const alt = item.altDescription
+    ? item.altDescription
+    : item.description
+    ? item.description
+    : 'photograph';
+
+  const artist = `${item.artistNameFirst} ${item.artistNameLast}`;
+
   return (
     <>
       <figure>
-        <img src={ } alt={ } />
-        <figcaption>{ }</figcaption>
+        <img src={item.imageUrlSm} alt={`${alt} by ${artist}`} />
+        <figcaption>{`${alt} by ${artist}`}</figcaption>
       </figure>
     </>
   );
